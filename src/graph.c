6d46fb8a0dd550054df19523018399e1486bcd76
Sand Heward <sandhewa@ga-m4.msk.21-school.ru>
2022-07-23 20:53:35 +0300

    First

--- src/graph.c
+++ src/graph.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#define columns 25
+#define rows 80
 
+void func_print(int *matrix[columns][rows]);
+
+int main() {
+    int matrix[columns][rows];
+    func_print(matrix[columns][rows]);
+
+return 0;
+}
+
+void func_print(int *matrix[columns][rows]) {
+    for (int i = 0; i < columns; i++) {
+        for (int j = 0; j < rows; j++) {
+                printf(".");
+            } else {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+}
