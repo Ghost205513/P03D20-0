4bd56e675957babb98a8ea7119613fb619ad14ca
Lucio Anastasia <lucioana@student.21-school.ru>
2022-07-24 20:27:01 +0000

    Upload New File

---
+++ src/stack.h
@@ -0,0 +1,15 @@
+#ifndef SRC_STACK_H_
+#define SRC_STACK_H_
+
+#include <stdlib.h>
+
+struct stack {
+    double data;
+    struct stack *next;
+};
+
+struct stack *push(struct stack *root, double data);
+struct stack *pop(struct stack *root);
+void free_stack(struct stack *root);
+
+#endif  // SRC_STACK_H_
