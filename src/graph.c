5d558fb3083995838b1fb0734908e4da31d67e93
Lucio Anastasia <lucioana@student.21-school.ru>
2022-07-24 17:48:04 +0000

    Update graph.c

--- src/graph.c
+++ src/graph.c
@@ -24,18 +24,33 @@ int main() {
     return 0;
 }
 
-int input(char **t) {
-    int size;
-    size = 0;
-    int flag;
-    *t = malloc(sizeof(char));
-    if ( t == NULL)
-        flag = 0;
-    while(1) {
-        int s;
-        s = getchar();
-        if (s < '0' || s > '9') {
-            if ( strchr())
+char *readline_() {
+    char buf[81] = {0};
+    char *res = NULL;
+    int len = 0;
+    int n = 0;
+
+    do {
+        n = scanf("%80[^\n]", buf);
+        if (n < 0) {
+            if (!res) {
+                return NULL;
+            }
+        } else if (n > 0) {
+            int chunk_len = (int) strlen(buf);
+            int str_len = len + chunk_len;
+            res = realloc(res, str_len + 1);
+            memcpy(res + len, buf, chunk_len);
+            len = str_len;
+        } else {
+            scanf("%*c");
         }
+    } while (n > 0);
+
+    if (len > 0) {
+        res[len] = '\0';
+    } else {
+        res = calloc(1, sizeof(char));
     }
+    return res;
 }
